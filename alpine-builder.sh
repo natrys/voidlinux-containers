8e6dce848ab8f38c5f316bacc42d3414cffa9093
Imran Khan <imran@khan.ovh>
2023-10-29 17:02:08 +0600

    update mirror, alpine version

--- alpine-builder.sh
+++ alpine-builder.sh
@@ -7,7 +7,7 @@ source lib/functions.sh # Brings in optparse(), usage(), die(), and bud() functi
 optparse "$@"
 
 # Build a builder from alpine
-alpine=$(buildah from alpine:3.17) || die 1 "Could not get alpine base image"
+alpine=$(buildah from alpine:3.18) || die 1 "Could not get alpine base image"
 # Do not remove build containers if we're debugging
 if [ -z "$BUILDAH_DEBUG" ]
 then
@@ -17,7 +17,7 @@ alpine_mount=$(buildah mount "$alpine") || die 2 "Could not mount alpine image, 
 
 bud copy "$alpine" void-mklive/keys/* /target/var/db/xbps/keys/
 bud run "$alpine" -- apk add ca-certificates curl
-wget -O- "https://alpha.de.repo.voidlinux.org/static/xbps-static-latest.$(uname -m)-musl.tar.xz" | tar Jx -C "$alpine_mount"
+wget -O- "https://repo-default.voidlinux.org/static/xbps-static-latest.$(uname -m)-musl.tar.xz" | tar Jx -C "$alpine_mount"
 XBPS_ARCH=$ARCH
 export XBPS_ARCH
 echo $REPO_MUSL
